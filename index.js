82bdd0b6b1761d61deb83ec29fb9c888e7a72c0c
R-Mehdi94 <mehdi.bellilpro@gmail.com>
2024-11-28 11:49:38 +0100

    test non validé

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function jeterDes(des){
+    if(des[0] == des[1] == des[2] == des[3] == des[4]){
+        return 50;
+    } 
     return des[0] + des[1] + des[2] + des[3] + des[4]
 }
 
--- index.test.js
+++ index.test.js
@@ -3,6 +3,11 @@ const jeterDes = require(".")
 describe('jeterDes', () =>{
     it.each([
         {input:[1,1,2,2,3],output:9},
+        {input:[1,1,4,3,3],output:12},
+        {input:[1,1,1,1,1],output:50},
+        {input:[3,3,3,3,3],output:50},
+
+
     ])("should return $output when given $input", ({input, output}) => {
         expect(jeterDes(input)).toBe(output);
     });
