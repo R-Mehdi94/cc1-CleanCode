b37f7ff30543f9f8ad479a18f7daa53941d8231a
R-Mehdi94 <mehdi.bellilpro@gmail.com>
2024-11-28 12:11:12 +0100

    Brelan

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 function jeterDes(des){
 
-
-
     if(des.every(element => element === des[0])){
         return 50;
     } 
+
+    let desResultat = {}
+
+    
+
+    for(let i = 0; i<des.length; i++){
+        const valeur = des[i];
+        if(desResultat[valeur] == null){
+            desResultat[valeur] = 1;
+        }else{
+            desResultat[valeur] +=1;
+        }
+    }
+
+    for(let resultat in desResultat){
+        if (desResultat[resultat] == 3){
+            return 28
+        }   
+    }
+
 
     if(des[0] + des[1] + des[2] + des[3] + des[4] == 15 | des[0] + des[1] + des[2] + des[3] + des[4] == 20){
         return 40
     }
 
-    
+
     return des[0] + des[1] + des[2] + des[3] + des[4]
 }
 
--- index.test.js
+++ index.test.js
@@ -8,6 +8,8 @@ describe('jeterDes', () =>{
         {input:[3,3,3,3,3],output:50},
         {input:[1,2,3,4,5],output:40},
         {input:[2,3,4,5,6],output:40},
+        {input:[1,1,1,5,6],output:28},
+
 
 
 
