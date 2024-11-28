243cf8332a9c1bc921b75179c8f4fe31f347e006
R-Mehdi94 <mehdi.bellilpro@gmail.com>
2024-11-28 11:58:37 +0100

    Grande suite

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function jeterDes(des){
 
+
+
     if(des.every(element => element === des[0])){
         return 50;
     } 
+
+    if(des[0] + des[1] + des[2] + des[3] + des[4] == 15 | des[0] + des[1] + des[2] + des[3] + des[4] == 20){
+        return 40
+    }
+
+    
     return des[0] + des[1] + des[2] + des[3] + des[4]
 }
 
--- index.test.js
+++ index.test.js
@@ -6,6 +6,9 @@ describe('jeterDes', () =>{
         {input:[1,1,4,3,3],output:12},
         {input:[1,1,1,1,1],output:50},
         {input:[3,3,3,3,3],output:50},
+        {input:[1,2,3,4,5],output:40},
+        {input:[2,3,4,5,6],output:40},
+
 
 
     ])("should return $output when given $input", ({input, output}) => {
